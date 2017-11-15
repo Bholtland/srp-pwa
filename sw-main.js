9b8b71be97f8d61ee15db97307b9eb352118aa74
Berend <berendholtland@hotmail.com>
2017-11-15 20:23:51 +0100

    test offline

--- sw-main.js
+++ sw-main.js
@@ -18,7 +18,7 @@ self.addEventListener('fetch', function(event) {
     caches.match(event.request)
       .then(function(response) {
         if (response) {
-          console.log('offline')
+          alert('offline')
           return response;
         }
         return fetch(event.request);
